849916d037367215e8b3d1910f20bf89b46c2821
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-04-12 13:05:07 +0200

    VFS-1825 slightly rework resolve-links script

--- resolve-links.py
+++ resolve-links.py
@@ -24,22 +24,29 @@ def_gui_src_path = os.path.join(script_dir, 'oz-gui-default', 'src')
 src_path = os.path.join(script_dir, 'src')
 
 checked_files = {}
-links_ok = 0
-links_added = 0
-links_broken = 0
-broken_links = []
-overrides_detected = 0
+count_links_ok = 0
+count_links_added = 0
+count_links_broken = 0
+links_broken = []
+count_overrides = 0
 overrides = []
-homepage_files = 0
+count_homepage_files = 0
+
+# Excluded directories
+exclude = set(['node_modules', 'bower_components', 'tmp'])
 
 # Walk the oz-gui-default dir and resolve links
-for root, _, files in os.walk(def_gui_src_path):
+for root, dirs, files in os.walk(def_gui_src_path, topdown=True):
+    # Skip excluded dirs
+    dirs[:] = [d for d in dirs if d not in exclude]
     for file in files:
         # Absolute path to file in oz-gui-default
         abs_file_path = os.path.join(root, file)
         # Relative path to file in oz-gui-default
         # (relative to oz-gui-default/src)
         rel_file_path = os.path.relpath(abs_file_path, def_gui_src_path)
+        # If
+
         # Corresponding path in src dir
         path_to_check = os.path.join(src_path, rel_file_path)
         checked_files[path_to_check] = True
@@ -56,10 +63,10 @@ for root, _, files in os.walk(def_gui_src_path):
                              os.readlink(path_to_check)))
             # Check if this link's target is the file in oz-gui-default
             if link_target == abs_file_path:
-                links_ok += 1
+                count_links_ok += 1
             else:
-                links_broken += 1
-                broken_links.append(rel_file_path)
+                count_links_broken += 1
+                links_broken.append(rel_file_path)
                 # print(link_target)
                 # print(os.path.abspath(link_target))
                 # print(abs_file_path)
@@ -67,7 +74,7 @@ for root, _, files in os.walk(def_gui_src_path):
         # This file is a file
         elif os.path.isfile(path_to_check):
             # print('file')
-            overrides_detected += 1
+            count_overrides += 1
             overrides.append(rel_file_path)
 
         # This file does not exist
@@ -78,43 +85,45 @@ for root, _, files in os.walk(def_gui_src_path):
             if not os.path.isdir(link_dir):
                 os.makedirs(link_dir)
             os.symlink(link_target, path_to_check)
-            links_added += 1
+            count_links_added += 1
 
             # print(link_target, path_to_check)
             # print('nothing')
 
 # Walk the src dir and look for paths that were not checked
-for root, _, files in os.walk(src_path):
+for root, dirs, files in os.walk(src_path, topdown=True):
+    # Skip excluded dirs
+    dirs[:] = [d for d in dirs if d not in exclude]
     for file in files:
         # Absolute path to file in src
         abs_file_path = os.path.join(root, file)
         # Relative path to file in src
         rel_file_path = os.path.relpath(abs_file_path, src_path)
         if not abs_file_path in checked_files:
-            homepage_files += 1
+            count_homepage_files += 1
             if os.path.islink(abs_file_path):
                 if not os.path.isfile(os.readlink(abs_file_path)):
-                    links_broken += 1
-                    broken_links.append(rel_file_path)
+                    count_links_broken += 1
+                    links_broken.append(rel_file_path)
 
 
 print('Finished!')
 print('-----------------------------------------------')
 print('')
-print('Links OK:         {0}'.format(links_ok))
+print('Links OK:         {0}'.format(count_links_ok))
 print('')
-print('Links added:      {0}'.format(links_added))
+print('Links added:      {0}'.format(count_links_added))
 print('')
 print('Homepage files:   {0}   (files/links specific only for homepage)'.format(
-    homepage_files))
+    count_homepage_files))
 print('')
 print(
 'Overriden files:  {0}   (files from default GUI that were overriden)'.format(
-    overrides_detected))
+    count_overrides))
 for override in overrides:
     print('   \033[92m{0}\033[0m'.format(override))
 print('')
-print('Broken links:     {0}   (should be fixed manually)'.format(links_broken))
-for broken_link in broken_links:
+print('Broken links:     {0}   (should be fixed manually)'.format(count_links_broken))
+for broken_link in links_broken:
     print('   \033[91m{0}\033[0m'.format(broken_link))
 print('')
