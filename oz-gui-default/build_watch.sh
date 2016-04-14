bd055e1002ead8b337db018d3d869dba8da32a74
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-04-14 21:10:46 +0200

    VFS-1825 remove oz-gui-default subtree

--- oz-gui-default/build_watch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_DIR=src
-REL_DIR=rel
-
-cd ${SRC_DIR} && ember build --watch --output-path=../${REL_DIR}
--- oz-gui-default/find_broken_links.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find . -type l -exec sh -c "file -b {} | grep -q ^broken" \; -print
