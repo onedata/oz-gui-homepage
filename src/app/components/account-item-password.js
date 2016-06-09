6d81d7218c606074a8c27bf93d5df8be58037faa
Jakub Liput <jakub.liput@gmail.com>
2016-06-09 20:24:01 +0200

    Squashed 'oz-gui-default/' changes from bef6c0e..efe95a5
    efe95a5 Merge pull request #14 in VFS/oz-gui-default from feature/VFS-2131-frontend-username-auth-2 to develop
    f5c7a6a VFS-2131 Fixing test mock
    4fb5aee Merge pull request #13 in VFS/oz-gui-default from feature/VFS-2131-frontend-username-auth-2 to develop
    ade94ea VFS-2131 Cleaning up
    d49e320 VFS-2131 Message on new passwords not match
    b1b18c1 VFS-2131 Change times of success messages in modals
    2d3778e VFS-2131 Show "change password" only if basic auth is enabled for user
    0e4524e VFS-2131 Improvements and fixes in basicAuth login and password chane
    8d6d1db VFS-2131 Message on success in login with password form
    742da05 Merge commit '75726419bebaca1419eaab9d76e26a0ca8c10580' into feature/VFS-2131-frontend-username-auth-2
    7572641 Squashed 'gui/' changes from 61812a8..05c37db
    ac70246 VFS-2131 Indigo login button; improved login buttons resposiveness
    0133158 Squashed 'gui/' changes from f6cc81e..61812a8
    a078c77 Merge commit '0133158559a71c2fcd01cc8d17d8583690f97391' into feature/VFS-2131-frontend-username-auth-2
    ba919a7 VFS-2131 Disabling console.debug in production
    dc194cf VFS-2131 Change password GUI and username login styles
    04bf8ee VFS-2131 Refactored Onezone API (as in backend)
    c08e8d1 VFS-2131 Passing jsHint
    67435dd VFS-2131 "Login with username" button and modal
    e6ce5b3 Squashed 'gui/' changes from eb7bd53..f6cc81e
    d9ebccc Merge commit 'e6ce5b3d92f08dd9314be45de3889429e7c838b8' into feature/VFS-2131-frontend-username-auth-2
    cdbcc9f Merge commit 'dea67b36f98098c2358b0e0af47821c3024352f6' into develop
    dea67b3 Squashed 'gui/' changes from d0e5c3b..eb7bd53
    ecc07a5 Merge pull request #12 in VFS/oz-gui-default from feature/VFS-2068-batch-operations-in-websocket-protocol to develop
    55dd4bd Squashed 'gui/' changes from cfd9705..d0e5c3b
    cd855a9 Merge commit '55dd4bdd2ae24c6d7e515bd488bcf34f77a9ea6b' into feature/VFS-2068-batch-operations-in-websocket-protocol
    
    git-subtree-dir: oz-gui-default
    git-subtree-split: efe95a52b1e388f95ae24987b055d226cee8a50a

---
+++ src/app/components/account-item-password.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import PromiseLoadingMixin from '../mixins/promise-loading';
+
+/**
+ * A change account password button, which shows modal with password change form.
+ * @module components/account-item-password
+ * @author Jakub Liput
+ * @copyright (C) 2016 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+export default Ember.Component.extend(PromiseLoadingMixin, {
+  onezoneServer: Ember.inject.service('onezoneServer'),
+  classNames: ['account-item'],
+  classNameBindings: ['isLoading:sidebar-item-is-loading'],
+
+  isLoading: false,
+  authProviders: null,
+
+  click() {
+    this.set('modalOpened', true);
+  }
+});
