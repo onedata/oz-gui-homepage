b109a519908e4486004dd5ffd9ee51474da82bfb
Jakub Liput <jakub.liput@gmail.com>
2016-07-19 15:54:49 +0200

    Squashed 'oz-gui-default/' changes from ed1a3f3..8aad2b4
    8aad2b4 Adding status-panel from gui repo
    24cf6f0 Merge commit 'd81995ad8d4b6d104204f31a9c02d57aa3731c18' into develop
    d81995a Squashed 'gui/' changes from b89c605..1549d23
    34747f1 VFS-2260 Protect from provider invalid lat./lon.
    9461813 VFS-2330 Rename Client token -> Access token
    cd38756 Merge pull request #17 in VFS/oz-gui-default from feature/VFS-2148-websocket-failure-handler-exp1 to develop
    
    git-subtree-dir: oz-gui-default
    git-subtree-split: 8aad2b4109f2a605c7fc1014a896cc0976fdf8ba

--- src/app/models/clienttoken.js
+++ src/app/models/clienttoken.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 /**
- * A client token. It uses only id, which is given from server.
+ * A client token (access token). It uses only id, which is given from server.
  * @module models/clienttoken
  * @author Jakub Liput
  * @copyright (C) 2016 ACK CYFRONET AGH
