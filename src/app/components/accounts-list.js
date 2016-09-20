720cd3ad59db0c6345d5526b9e894ca0e6a65fbc
Jakub Liput <jakub.liput@gmail.com>
2016-09-20 15:53:05 +0200

    VFS-2626 Improved responsive design for login view for 8+ login btns

--- src/app/components/accounts-list.js
+++ src/app/components/accounts-list.js
@@ -18,7 +18,4 @@ export default Ember.Component.extend({
 
   authorizersSorting: ['type', 'email'],
   authorizersSorted: Ember.computed.sort('authorizers', 'authorizersSorting'),
-
-  initAuth: function() {
-  }.observes('authorizers')
 });
