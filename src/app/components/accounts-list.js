9772c7f44724116dd6d71afebf7c0f0f611df875
Jakub Liput <jakub.liput@gmail.com>
2016-11-15 14:38:35 +0100

    VFS-2752 Hide add account button when it should be Hide when list of supported authorizers is empty

--- src/app/components/accounts-list.js
+++ src/app/components/accounts-list.js
@@ -9,6 +9,8 @@ import Ember from 'ember';
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 export default Ember.Component.extend({
+  onezoneServer: Ember.inject.service(),
+
   classNames: ['accounts-list', 'accordion-content', 'sidebar-list'],
 
   /** List of authorizers objects for account-items, Objects with: type, email */
@@ -18,4 +20,32 @@ export default Ember.Component.extend({
 
   authorizersSorting: ['type', 'email'],
   authorizersSorted: Ember.computed.sort('authorizers', 'authorizersSorting'),
+
+  __supportedAuthorizersInitialized: false,
+
+  loadingSupportedAuthorizers: Ember.computed('__supportedAuthorizersInitialized', function() {
+    return !this.get('__supportedAuthorizersInitialized');
+  }),
+
+  noSupportedAuthorizers: Ember.computed('supportedAuthorizers.[]', function() {
+    let supportedAuthorizers = this.get('supportedAuthorizers');
+    return !supportedAuthorizers || supportedAuthorizers.length <= 0;
+  }),
+
+  passwordConfigEnabled: Ember.computed('session.sessionDetails.basicAuthEnabled', function() {
+    return this.get('session.sessionDetails.basicAuthEnabled') === true;
+  }),
+
+  initSupportedAuthorizers: Ember.on('init', function() {
+    let promise = this.get('onezoneServer').getSupportedAuthorizers();
+    promise.then(data => {
+      this.set('supportedAuthorizers', data.authorizers);
+    });
+    promise.catch(error => {
+      console.error(`Cannot fetch supportedAuthorizers: ${error.message}`);
+    });
+    promise.finally(() => {
+      this.set('__supportedAuthorizersInitialized', true);
+    });
+  }),
 });
