6329fdff05cbfd396b35a536c3872e182d892f26
Jakub Liput <jakub.liput@gmail.com>
2016-12-16 14:34:06 +0100

    VFS-2883 Support sizes of spaces in sidebar

--- src/app/components/provider-place-drop-space.js
+++ src/app/components/provider-place-drop-space.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ProviderSpaceComponentMixin from 'oz-worker-gui/mixins/components/provider-space';
 
 /**
  * A list entry for single space in context of particular provider
@@ -11,37 +12,6 @@ import Ember from 'ember';
  * @copyright (C) 2016 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
-export default Ember.Component.extend({
-  onezoneServer: Ember.inject.service(),
-  messageBox: Ember.inject.service(),
-  notify: Ember.inject.service(),
-  
-  tagName: 'li',
-  classNames: ['provider-place-drop-space'],
-  
-  /**
-   * To inject.
-   * @type {String}
-   * @required
-   */
-  providerId: null,
-
-  /**
-   * To inject.
-   * @type {Space}
-   * @required
-   */
-  space: null,
-
-  name: Ember.computed.alias('space.name'),
-
-  supportSize: Ember.computed('space.supportSizes', 'providerId', function() {
-    let supportSizes = this.get('space.supportSizes');
-    let providerId = this.get('providerId');
-    return supportSizes[providerId];
-  }),
-  
-  iconName: Ember.computed('space.isDefault', function() {
-    return this.get('space.isDefault') ? 'space-default' : 'space';
-  })
+export default Ember.Component.extend(ProviderSpaceComponentMixin, {  
+  classNames: ['provider-place-drop-space']
 });
