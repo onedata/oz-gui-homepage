2a101fe9a4518c050328bcfc3c8417f1c43e293e
Jakub Liput <jakub.liput@gmail.com>
2017-01-04 11:36:51 +0100

    VFS-2882 Fixing mocha tests deprecations

--- src/tests/unit/components/one-radio-button-test.js
+++ src/tests/unit/components/one-radio-button-test.js
@@ -1,25 +1,20 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import {
-  describeComponent,
-  it
-} from 'ember-mocha';
+import { it, describe } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
 
-describeComponent(
-  'one-radio-button',
-  'OneRadioButtonComponent',
-  {
+describe('OneRadioButtonComponent', function() {
+  setupComponentTest('one-radio-button', {
     needs: ['component:one-icon'],
     unit: true
-  },
-  function() {
-    it('renders', function() {
-      // creates the component instance
-      let component = this.subject();
-      // renders the component on the page
-      this.render();
-      expect(component).to.be.ok;
-      expect(this.$()).to.have.length(1);
-    });
-  }
-);
+  });
+
+  it('renders', function() {
+    // creates the component instance
+    let component = this.subject();
+    // renders the component on the page
+    this.render();
+    expect(component).to.be.ok;
+    expect(this.$()).to.have.length(1);
+  });
+});
